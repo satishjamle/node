e1954c8f7096a8f58ffcf16be3dc95e4cdaa441d
satishjamle <satishjamle513@gmail.com>
2025-01-18 22:53:29 +0530

    Express -05 ( pug template engine)

--- node/Express/app.js
+++ node/Express/app.js
@@ -1,44 +1,41 @@
-const express = require("express") // import express'
-const path = require('path');
-const app = express()              // create app 
-const port = 80                   // port run on 80
+const express = require("express"); // import express'
+const path = require("path");
+const fs = require("fs");
+const app = express(); // create app
+const port = 8080; // port run on 8000
 
-// for serving static files
+//EXPRESS SPECIFIC STUFF
 
-app.use('/static' , express.static('static'))
+app.use("/static", express.static("static"));
+app.use(express.urlencoded());
 
-// set the template engine as pug
-app.set('view engine' ,'pug')
+// PUG SPECIFIC STUFF
+app.set("view engine", "pug");
 
 // set the views directory
-app.set('views',path.join(__dirname, 'views'))
+app.set("views", path.join(__dirname, "views"));
 
-// our pug demo endpoint
-app.get("/demo" ,(req,res)=>{
-    res.status(200).render('demo', { title: 'Hey', message: 'Hello there and thanks for telling me how to use pubg' })
+// ENDPOINTS
+app.get("/", (req, res) => {
+  const con = "this is the best cntent on the internet so far so use it wisely";
+  const params = { title: "pubg is the best game", content: con };
+  res.status(200).render("index.pug", params);
+});
+app.post("/", (req, res) => {
+  name = req.body.name;
+  age = req.body.age;
+  gender = req.body.gender;
+  address = req.body.address;
+  more = req.body.more;
 
-})
+  let outputtowrite = `the name of the client is ${name} , ${age} years old , ${gender} , residing at ${address} . more about him/ her : ${more}`;
+  fs.writeFileSync("output.txt", outputtowrite);
+  const params = { message: "your form has been submitted successfully" };
 
-
-app.get("/",(req,res)=>{             // handle of get request / end point pr
-    res.send("this is my homepage of my first express app with harry")
-
+  res.status(200).render("index.pug", params);
 });
 
-app.get("/about" ,(req,res)=>{            // handle of get request /about end point pr
-    res.send("this is my about page express app with user")
-
-})
-app.post("/contact" ,(req,res)=>{             //// i want to post /contact handler pr of get request / end point pr
-    res.send("ths is a post request contact page of my first express app with harry ")
-})
-;
-app.post("/page" ,(req,res)=>{             // status ko hit krna ho tho
-    res.status(404).send("this page is not found ")
-})
-
-app.listen(port ,()=>{            //app listen on port and response
-    console.log(`the application started successfully on port ${port}`)
-
-})
-
+// START THE SERVER
+app.listen(port, () => {
+  console.log(`Server is running on successfully on port :${port}`);
+});
